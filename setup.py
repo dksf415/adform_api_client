cc8638f2a542fbc02b2d46af012b7ed85765b014
Dean Kondo <dean.kondo@annalect.com>
2019-08-23 13:28:13 -0700

    add setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="amobeeclient",
+    version="0.1.dev0",
+    author="Dean Kondo",
+    author_email="deankondo@annalect.com",
+    description="Simple wrapper for AdForm API",
+    url="https://github.com/accuenmedia/adform-api-client",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6.7",
+        "License :: GNU GENERAL PUBLIC LICENSE",
+        "Operating System :: OS Independent",
+    ],
+)
