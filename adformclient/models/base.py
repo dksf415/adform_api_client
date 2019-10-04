4bca86d33e3e2575cd20ebfb10d9da6b6858462d
Dean Kondo <dean.kondo@annalect.com>
2019-10-04 13:43:55 -0700

    Get campaign by id

--- adformclient/models/base.py
+++ adformclient/models/base.py
@@ -94,3 +94,13 @@ class Base:
             rval["data"] = data
 
         return json.dumps(rval)
+
+    def get_by_id(self, id):
+        """
+        :param id:
+        :return: JSON object
+        """
+        url = "{0}/{1}/{2}".format(self.url_metadata, self.object, id)
+        response = self.make_request("GET", url)
+
+        return self.get_response_object(response)
