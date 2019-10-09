ef0870b5ab3a5aada37e83fbc4e8754856e18ce4
Dean Kondo <dean.kondo@annalect.com>
2019-10-09 15:18:07 -0700

    Add get by line item id

--- adformclient/models/line_item.py
+++ adformclient/models/line_item.py
@@ -20,4 +20,16 @@ class LineItem(Base):
 
         return self.get_response_list(response.text, response.status_code)
 
+    def find_by_id(self, id):
+        """
+
+        :param id:
+        :return: JSON array
+        """
+        scope = 'https://api.adform.com/scope/buyer.rtb.lineitem'
+        url = "{}/rtb/lineitems/{}".format(self.url_metadata, id)
+        response = self.make_request("GET", url, scope)
+
+        return self.get_response_list(response.text, response.status_code)
+
         pass
